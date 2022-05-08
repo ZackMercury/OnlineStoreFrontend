05af1a908be42810be0f5f2101a7b685864c4eb3
ZackMercury <zackmercury@hotmail.com>
2022-05-08 03:08:36 +0300

    Setting up webpack and configuring babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,38 @@ module.exports = {
     entry: './src/index.ts',
     
     output: {
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "dst"),
         filename: "bundle.js"
     },
 
     module: {
         rules: [
             {
-                test: /\.(js|ts)x?/,
-                exclude: [
-                    path.resolve("node_modules")
-                ]
+                test: /\.(js|ts)x?$/,
+                include: [
+                    path.resolve(__dirname, "src")
+                ],
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            [
+                                "@babel/preset-env", 
+                                {
+                                    targets: "defaults"
+                                }
+                            ],
+                            "@babel/preset-typescript",
+                            "@babel/preset-react"
+                        ]
+                    }
+                }
+                
             }
         ]
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"]
     }
 }
